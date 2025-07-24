57b8f7364091802820072aeb37a11a1b6bc671c3
GBIS <78549430+GBIS-Lab@users.noreply.github.com>
2025-07-24 10:36:35 +0200

    Update H2DbCleaner.java

--- src/main/java/com/smartphonebatch/utile/H2DbCleaner.java
+++ src/main/java/com/smartphonebatch/utile/H2DbCleaner.java
@@ -8,7 +8,7 @@ import java.io.File;
 @Component
 public class H2DbCleaner {
 
-    private static final String DB_DIR = "data"; // ou le chemin absolu complet si besoin
+    private static final String DB_DIR = "data"; 
     private static final String[] FILES_TO_DELETE = {
             "testdb.mv.db",
             "testdb.trace.db"
